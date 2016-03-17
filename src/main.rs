a66deb61b8b54c56d3b90706c1278100f6d8aed3
Andrey Ivanov <lekkion@gmail.com>
2016-03-17 18:29:55 +0400

    new version

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,32 @@
-#![feature(io)]
-extern crate hyper;
-extern crate mysql;
-extern crate html5ever;
+extern crate regex;
 
-use std::default::Default;
-use std::io::Read;
-use std::string::String;
-use std::str::from_utf8;
+use regex::Regex;
 
-use hyper::Client;
-use hyper::header::Connection;
 
-use mysql::conn::MyOpts;
-use mysql::conn::pool::MyPool;
-use mysql::value::{from_value, from_row};
-
-use html5ever::driver::parse;
-
-struct News {
-    id: String,
-    title: String
+struct Text<'a> {
+    origin: &'a str,
+    temp: String
 }
+
+impl<'a> Text<'a> {
+    fn canonize(&mut self) {
+        let html = Regex::new(r"<[^>]*>|[:punct:]").unwrap();
+        self.temp = html.replace_all(self.origin, " ");        
+    }
+}
+
+
+static TEXT1: &'static str = "Сегодня, 27 октября, на сессии Госсовета Удмуртии мандат Дмитрия Кулишова, который сложил полномочия для того, чтобы перейти в Городскую Думу Ижевска, был передан Геннадию Зарубежнову – «зарегистрированному кандидату из республиканского списка кандидатов выдвинутого избирательным объединением удмуртское региональное отделение политической партии ЛДПР». По данным ЦИК УР, Геннадий Зарубежнов (1962 г.р.) является преподавателем ОУ ССПО «Нефтяной техникум», а также депутатов Совета депутатов Завьяловского района на непостоянной основе. На сайте Завьяловского района также указано, что он работает заместителем директора группы компаний «Прогресс».";
+static TEXT2: &'static str = "Геннадий Зарубежнов стал депутатом Госсовета Удмуртии. Он получил мандат, освободившийся после перехода руководителя фракции ЛДПР Дмитрия Кулишова в Гордуму Ижевска. Зарубежнов шёл по спискам ЛДПР на выборах 2012 года. Сегодня на сессии республиканского парламента ему вручили удостоверение и знак депутата. Геннадий Зарубежнов занимает пост директора фирмы Прогресс, до недавнего времени являлся депутатом совета депутатов Завьяловского район";
 
 fn main() {
-    let opts = MyOpts {
-        user: Some("root".to_string()),
-        db_name: Some("news18".to_string()),
-//        tcp_addr: Some("127.0.0.1".to_string()),
-        ..Default::default()
+    let mut first = Text {
+        origin: TEXT1,
+        temp: String::new()
     };
 
-    let pool = MyPool::new(opts)
-        .ok()
-        .expect("Connect to db failed");
+    first.canonize();
 
-//    let collect_news: Vec<News> = pool.prep_exec("SELECT id, title from news limit 5", ()).map(|result| {
-//        result.map(|row| {
-//            let (id, title) = from_row(row.unwrap());
-//            News {
-//                id: id,
-//                title: title,
-//            }
-//        }).collect()
-//    }).unwrap();
+    println!("{}", first.temp);
 
-//    println!("{}", collect_news[1].title);
-
-
-    // Create a client.
-    let client = Client::new();
-
-    // Creating an outgoing request.
-    let mut res = client.get("http://www.myudm.ru/news/archive/6").send()
-        .ok()
-        .expect("Failed to get data");
-
-    // Read the Response.
-    let mut buf: Vec<u8> = Vec::new();
-    let mut string = String::new();
-
-//    match res.read_to_end(&mut buf) {
-//        Ok(r) => println!("{:?}", r),
-//        Err(e) => println!("{:?}", e),
-//    };
-
-//    let chunks = buf.chunks(1000);
-//
-//    for el in chunks {
-//        match from_utf8(el) {
-//            Ok(r) => println!("{:?}", r),
-//            Err(e) => println!("{:?}", e),
-//        }
-//    }
-
-    match res.read_to_string(&mut string) {
-        Ok(r) => println!("{:?}", r),
-        Err(e) => println!("{:?}", e),
-    }
-
-    println!("{}", string);
-
-
-//    for c in res.chars() {
-//        println!("{}", c.unwrap());
-//    }
 }
-
-
-
